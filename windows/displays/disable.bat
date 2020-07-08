a513fe30e7575ec556fbf378d848e21c5848220a
Ilya Fateev <fat32nov@gmail.com>
2020-07-08 15:43:35 +0300

    set primary display

--- windows/displays/disable.bat
+++ windows/displays/disable.bat
@@ -1,3 +1,4 @@
 multimonitortool /enable MONITOR\SAM0350\{4d36e96e-e325-11ce-bfc1-08002be10318}\0012
 multimonitortool /enable MONITOR\VSCC132\{4d36e96e-e325-11ce-bfc1-08002be10318}\0014
+multimonitortool /SetPrimary MONITOR\VSCC132\{4d36e96e-e325-11ce-bfc1-08002be10318}\0014
 multimonitortool /disable MONITOR\Default_Monitor\{4d36e96e-e325-11ce-bfc1-08002be10318}\0015
