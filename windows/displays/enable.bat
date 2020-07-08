73112bfa914f3e2f8c8120fdeeb9772628f61ea3
Ilya Fateev <fat32nov@gmail.com>
2020-07-08 15:40:07 +0300

    Update disable.bat and enable.bat

---
+++ windows/displays/enable.bat
@@ -0,0 +1,3 @@
+multimonitortool /enable MONITOR\Default_Monitor\{4d36e96e-e325-11ce-bfc1-08002be10318}\0015
+multimonitortool /disable MONITOR\SAM0350\{4d36e96e-e325-11ce-bfc1-08002be10318}\0012 MONITOR\VSCC132\{4d36e96e-e325-11ce-bfc1-08002be10318}\0014
+
